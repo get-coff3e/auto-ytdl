e89991c90e9731b9864a8104688a4b22b592d4a1
coff3e <64768219+get-coff3e@users.noreply.github.com>
2022-04-04 17:42:49 +0000

    nuts

--- main.py
+++ main.py
@@ -4,30 +4,28 @@ import json
 import yt_dlp
 from urllib.parse import urlparse
 from datetime import datetime
+# rich is not required, but it looks nice
 #try:
 #    from rich import print
 #except:
 #    pass
-# rich is not required, but it looks nice
 
 INPUTFILE = 'channels.json'
 
 
 def fileValidation():
-    with open(INPUTFILE) as f:
-        if not os.path.exists(INPUTFILE):
-            print(f"{INPUTFILE} not found. Add entries in before continuing")
-            f.write('{\n\t"channels": [\n\t]\n}')
-            f.close()
-        c_load = json.load(f)
-        c = c_load['channels']
+    json_template = {"channels": []}
 
-        if not len(c):
-            print("No entries to download")
-            exit()
-        else:
-            print(f"# of Channels: {len(c)}")
-        return c
+    if not os.path.exists(INPUTFILE):
+        json.dump(json_template, open(INPUTFILE, 'w'), indent=4)
+    else:
+        with open(INPUTFILE) as f:
+            try:
+                c = json.load(f)['channels']
+                print(f"# of Channels: {len(c)}")
+                return c
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
@@ -36,28 +34,26 @@ if __name__ == "__main__":
 
     while len(c):
         currentHour = datetime.now().hour
-        
+
         if currentHour != storedHour:
             print(f"Time changed:\tHour {currentHour}:00:00")
             storedHour = datetime.now().hour
 
         for entry in c:
             website = urlparse(entry).hostname
-
             OUTTMPL = f"Download/{website}/%(channel)s/%(playlist)s/%(title)s/%(id)s.%(ext)s"
             YDL_OPTS = {
                 'format': 'bestvideo/best',
                 'outtmpl': OUTTMPL,
                 #'verbose': True,
                 'ignoreerrors': True,
-                'quiet': True,
+                #'quiet': True,
                 'writedescription': True,
                 'writeinfojson': True,
                 'writeannotations': True,
                 'writethubmnail': True,
                 'writesubtitles': True
             }
-
             with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
                 info_dict = ydl.extract_info(entry, download=False)
                 channel_name = info_dict.get("url", None)
